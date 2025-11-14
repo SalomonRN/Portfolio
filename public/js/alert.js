585e66959f08872db238732e67f7e97cc4c15212
SalomonRN <salomonrn20@gmail.com>
2025-11-14 17:20:06 -0500

    feat: add sweetalert dependency

---
+++ public/js/alert.js
@@ -0,0 +1,15 @@
+import Swal from "sweetalert2";
+const alertaProyecto = (url) => {
+  if (!url) return
+  fetch(url) // Awake server
+  Swal.fire({
+    title: "Aviso importante",
+    text: "Este proyecto está actualmente alojado en un servicio gratuito de Render, por lo que no está activo 24/7 y solo se inicia cuando se le solicita información. Debido a esto, está cargando en este momento y puede tardar un poco en estar disponible. Agradezco su comprensión. ¡Muchas gracias!",
+    color: "#E19A17",
+    background: "#050530",
+    grow: "column",
+    confirmButtonText: "Aceptar",
+  });
+};
+
+export default alertaProyecto
